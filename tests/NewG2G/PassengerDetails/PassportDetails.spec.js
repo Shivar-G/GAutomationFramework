3e4e207baafad442f6f3cc89e6809d0be3827e65
Shivar-G <shivar.ram@gadentures.com>
2024-06-21 11:48:34 +0200

    Adjustments

--- tests/NewG2G/PassengerDetails/PassportDetails.spec.js
+++ tests/NewG2G/PassengerDetails/PassportDetails.spec.js
@@ -6,22 +6,18 @@ const {test, expect} = require('@playwright/test');
 
 test('Login', async ({page})=>
 {
-   await page.goto("http://127.0.0.1:8000/g2g/login/");
-   await page.locator("(//span[@class='Select-arrow-zone'])[1]").click();
-   await page.keyboard.type('Decemember');
-   await page.waitForTimeout(2000);
-   await page.keyboard.press('Enter');
-   await page.locator("(//input[@type='text'])[2]").fill("27");
-   await page.locator("(//input[@type='text'])[3]").fill("1975");
-   await page.locator("(//span[@class='Select-arrow'])[2]").click();
-   await page.keyboard.type('British');
-   await page.waitForTimeout(2000);
-   await page.keyboard.press('Enter');
-   await page.waitForTimeout(2000);
-   await page.locator("(//button[normalize-space()='Login'])[1]").click();
-   await page.waitForTimeout(35000);
-   await page.locator("(//span[normalize-space()='Close'])[1]").click();
-   await page.waitForTimeout(10000);
-   await page.locator("(//a[normalize-space()='Joey Byford'])[1]").click();
-   await page.waitForTimeout(15000);
+    await page.goto("http://127.0.0.1:8000/g2g/login/");
+    const element = page.locator('#month');
+    await element.selectOption({value: '12'});
+    await page.waitForTimeout(2000);
+    await page.locator("#day").fill("27");
+    await page.locator("#year").fill("1975");
+    const dropDown = page.locator('#nationality');
+    await dropDown.selectOption({value: 'BRITISH'});
+    await page.waitForTimeout(2000);
+    await page.locator("//button[@id='login-button']").click();
+    await page.waitForTimeout(3000);
+    await page.locator("//a[normalize-space()='Ross Dalziel (4/9)']").click();
+    await page.waitForTimeout(10000);
+    await page.locator("//a[normalize-space()='Passport Information']").click();
 });
